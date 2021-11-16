f71aa535c431cd09e454a784447a604f7ea87004
Jada Allen <47389874+jallen3@users.noreply.github.com>
2021-11-16 12:42:03 -0500

    Updated Toggle Button

--- aquatracker/src/Settings.js
+++ aquatracker/src/Settings.js
@@ -20,7 +20,7 @@ export class Settings extends React.Component {
                     <div class="sliderWrapper">
                         <div>Temperature&nbsp;&nbsp;</div>
                         <label class="switch">
-                            <input type="checkbox"/>
+                            <input type="checkbox" checked/>
                             <span class="slider round"></span>
                         </label>
                     </div>
@@ -36,9 +36,9 @@ export class Settings extends React.Component {
 
                     <br></br>
                     <div class="sliderWrapper">
-                        <div>Maps&nbsp;&nbsp;</div>
+                        <div>Graphs&nbsp;&nbsp;</div>
                         <label class="switch">
-                            <input type="checkbox"/>
+                            <input type="checkbox" checked/>
                             <span class="slider round"></span>
                         </label>
                     </div>
